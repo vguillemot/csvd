5cfcd8fb3b5e8ee9f05d0dabd94369e728d93f44
Vincent Guillemot <vincentguillemot@hotmail.com>
2018-06-25 12:22:00 +0200

    Add group projection

--- R/normgroup.R
+++ R/normgroup.R
@@ -4,8 +4,8 @@
 #' @param g A group factor
 #' @return The \eqn{l_{1,2}} group norm of \eqn{u}: \eqn{\sum_g ||u_g||_2}.
 #' @examples 
-#' normg( c(0, 0, -0.5, 0.5), gl(2, 2) )
+#' normgroup( c(0, 0, -0.5, 0.5), gl(2, 2) )
 #' @export
-normg <- function(u, g) {
+normgroup <- function(u, g) {
   sum(tapply(X = u, INDEX = g, FUN = norm2))
 }
